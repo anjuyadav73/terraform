c5671dd7f1b1093fa5c58b31b631ed991fd8c9c6
anjuyadav73 <anjuyadav73@gmail.com>
2021-06-08 00:45:17 +0100

    commit 3

--- app1/main.tf
+++ app1/main.tf
@@ -3,13 +3,14 @@ module "ent-ec2-instance" {
   source                 = "../modules/ec2/"
   latest-ami-id          = "/GoldenAMI/RHEL-7/CISRHEL7Benchmark-1/latest"
   instance-type          = "t2.micro"
-  iam-instance-profile   = "DefaultSSMInstanceProfile"
+  iam-instance-profile   = "admin"
   vpc-security-group-ids = ["sg-018c79dbc3ed138b8"]
   subnet-id              = "subnet-705bdc1b"
   create-ebs-volume      = "true"
-  instance-count         = "2"
+  instance-count         = "1"
   ebs-volume-size        = "10"
-  user-data              = "IyEgL2Jpbi9iYXNoCnN1ZG8gYXdzIHMzIGNwIC0tcmVjdXJzaXZlIHMzOi8vcm9nZXJzLXVzZXJkYXRhLWludGVybmFsLXN0b3JhZ2UtMS9lYzJib290c3RyYXAuc2ggL3RtcC8Kc3VkbyBjaG1vZCA3NTUgL3RtcC9lYzJib290c3RyYXAuc2gKc291cmNlIC90bXAvZWMyYm9vdHN0cmFwLnNo"
+  user-data              = ""
+
 
   # Tagging variabless
   instance-name       = "demo-ent-rhel"
